817bbdaafa53c18a157600f275c85e347fe811d4
smchardie1 <mchardie.stella@gmail.com>
2025-10-14 16:20:55 +1300

    Maybe final code
    I added characters, made it when mouth is open they are on fire and when the go near open mouth they turn into skeletons. I also made it so when hands go off screen they are revived

--- Draw_interaction.js
+++ Draw_interaction.js
@@ -1,4 +1,4 @@
-// ----=  HANDS  =----
+// ----=  Image Variables  =----
 let bgImage;
 let villageImage;
 let villageFireImage;
@@ -7,16 +7,49 @@ let dragonBackImage;
 let dragonMouthImage;
 let dragonTopImage;
 let dragonFireImage;
+let farmerImage, farmerFireImage, farmerDeadImage;
+let chefImage, chefFireImage, chefDeadImage;
+let maidenImage, maidenFireImage, maidenDeadImage;
+let kidImage, kidFireImage, kidDeadImage;
+
+// ----= State Variables =----
+let lipDistance = 0;
+let farmerDead = false; 
+let chefDead = false;
+let maidenDead = false;
+let kidDead = false;
 
 function prepareInteraction() {
+  // Background
   bgImage = loadImage('/images/background.png');
+  // Village
   villageImage = loadImage('/images/village.png')
   villageFireImage = loadImage('/images/villagefire.png')
+  // Dragon
   dragonBodyImage = loadImage('/images/dragonbody.png')
   dragonBackImage = loadImage('/images/dragonback.png')
   dragonMouthImage = loadImage('/images/dragonmouth.png')
   dragonTopImage = loadImage('/images/dragontop.png')
   dragonFireImage = loadImage('/images/dragonfire.png')
+  // Farmer
+  farmerImage = loadImage('/images/farmer.png');
+  farmerFireImage = loadImage('/images/farmerfire.png');
+  farmerDeadImage = loadImage('/images/farmerdead.png');
+
+  // Chef
+  chefImage = loadImage('/images/chef.png');
+  chefFireImage = loadImage('/images/cheffire.png');
+  chefDeadImage = loadImage('/images/chefdead.png');
+
+  // Maiden
+  maidenImage = loadImage('/images/maiden.png');
+  maidenFireImage = loadImage('/images/maidenfire.png');
+  maidenDeadImage = loadImage('/images/maidendead.png');
+
+  // Kid
+  kidImage = loadImage('/images/kid.png');
+  kidFireImage = loadImage('/images/kidfire.png');
+  kidDeadImage = loadImage('/images/kiddead.png');
 }
 
 function drawInteraction(faces, hands) {
@@ -30,50 +63,50 @@ function drawInteraction(faces, hands) {
   //facePart
   // for loop to capture if there is more than one face on the screen. This applies the same process to all faces. 
   for (let i = 0; i < faces.length; i++) {
-    let face = faces[i]; // face holds all the keypoints of the face\
-    console.log(face);
-    if (showKeypoints) {
-      drawPoints(face)
-    }
-    noStroke();
+    let face = faces[i];
+    if (showKeypoints) drawPoints(face);
+
     let faceCenterX = face.faceOval.centerX;
     let faceCenterY = face.faceOval.centerY;
 
+    // Facial measurements
     let leftCheek = face.keypoints[234];
     let rightCheek = face.keypoints[454];
     let faceWidth = dist(leftCheek.x, leftCheek.y, rightCheek.x, rightCheek.y);
-    
+
     let upperLip = face.keypoints[13];
     let lowerLip = face.keypoints[14];
-    let lipDistance = dist(upperLip.x, upperLip.y, lowerLip.x, lowerLip.y);
+    lipDistance = dist(upperLip.x, upperLip.y, lowerLip.x, lowerLip.y);
     let mouthOffset = map(lipDistance, 0, 30, 0, 50);
     let fireOpacity = map(lipDistance, 0, 25, 0, 255);
-
-    
 
     push();
     imageMode(CENTER);
 
-    // make dragon body start at centre of head (top edge aligns with face centre)
+    // Dragon body (aligned to face)
     let bodyHeight = faceWidth * 2;
-    let bodyY = faceCenterY + bodyHeight / 2; // shifts body down so top is at face centre
-
+    let bodyY = faceCenterY + bodyHeight / 2;
     image(dragonBodyImage, faceCenterX, bodyY, faceWidth * 2, bodyHeight);
 
-    // rest of dragon parts
+    // Dragon parts
     image(dragonBackImage, faceCenterX, faceCenterY - 100, faceWidth * 2, faceWidth * 2);
     image(dragonMouthImage, faceCenterX, faceCenterY - 100 + mouthOffset, faceWidth * 2, faceWidth * 2);
 
+    // Draw the village
     village(face);
 
+    // Fire effect when mouth opens
     push();
     tint(255, fireOpacity);
     image(dragonFireImage, faceCenterX, faceCenterY + 100, faceWidth * 2, faceWidth * 2);
     pop();
-    
-    image(dragonTopImage, faceCenterX, faceCenterY - 100, faceWidth * 2, faceWidth * 2);
 
+    image(dragonTopImage, faceCenterX, faceCenterY - 100, faceWidth * 2, faceWidth * 2);
     pop();
+
+    // Save mouth position for collision detection
+    face.mouth = { x: faceCenterX, y: faceCenterY - 60, size: faceWidth };
+  
 
 
     /*
@@ -81,51 +114,129 @@ function drawInteraction(faces, hands) {
     */
 
   }
-
+  let leftHandPresent = false;
+  let rightHandPresent = false;
   // for loop to capture if there is more than one hand on the screen. This applies the same process to all hands.
   for (let i = 0; i < hands.length; i++) {
     let hand = hands[i];
+    let handType = hand.handedness; // "Left" or "Right"
+
     if (showKeypoints) {
-      drawPoints(hand)
-      drawConnections(hand)
+      drawPoints(hand);
+      drawConnections(hand);
     }
-    // console.log(hand);
-    let indexFingerTipX = hand.index_finger_tip.x;
-    let indexFingerTipY = hand.index_finger_tip.y;
-    /*
-    Start drawing on the hands here
-    */
 
-    // call puppet
-    drawPuppet(indexFingerTipX, indexFingerTipY);
+    // Get the finger tip coordinates
+    let indexX = hand.index_finger_tip.x;
+    let indexY = hand.index_finger_tip.y;
+    let pinkyX = hand.pinky_finger_tip.x;
+    let pinkyY = hand.pinky_finger_tip.y;
 
-    /*
-    Stop drawing on the hands here
-    */
+    if (faces.length === 0) continue;
+    let face = faces[0];
+    let mouth = face.mouth;
+
+    // -------- Left Hand --------
+    if (handType === "Left") {
+      leftHandPresent = true;
+      // Farmer (index) and Kid (pinky)
+      checkMouthCollision(indexX, indexY, 'farmer', mouth);
+      checkMouthCollision(pinkyX, pinkyY, 'kid', mouth);
+
+      farmerPuppet(indexX, indexY);
+      kidPuppet(pinkyX, pinkyY);
+    }
+
+    // -------- Right Hand --------
+    if (handType === "Right") {
+      rightHandPresent = true;
+      // Chef (index) and Maiden (pinky)
+      checkMouthCollision(indexX, indexY, 'chef', mouth);
+      checkMouthCollision(pinkyX, pinkyY, 'maiden', mouth);
+
+      chefPuppet(indexX, indexY);
+      maidenPuppet(pinkyX, pinkyY);
+    }
   }
-  function drawPuppet(x, y) {
-    push();
-    noStroke();
-    // body
-    fill(200, 150, 255);
-    ellipse(x, y + 40, 60, 80);
-    // head
-    fill(255, 220, 180);
-    ellipse(x, y, 50, 50);
-    // eyes
-    fill(0);
-    ellipse(x - 10, y - 5, 8, 8);
-    ellipse(x + 10, y - 5, 8, 8);
-    // mouth
-    noFill();
-    stroke(0);
-    strokeWeight(3);
-    arc(x, y + 5, 20, 15, 0, PI);
-    pop();
+  // Revive characters if hands go off-screen
+  if (!leftHandPresent) {
+    farmerDead = false;
+    kidDead = false;
+  }
+  if (!rightHandPresent) {
+    chefDead = false;
+    maidenDead = false;
+  }
+}
+// ---------------- Check if finger is in mouth ----------------
+function checkMouthCollision(fx, fy, character, mouth) {
+  let distToMouth = dist(fx, fy, mouth.x, mouth.y);
+  if (lipDistance > 15 && distToMouth < mouth.size / 2) {
+    if (character === 'farmer') farmerDead = true;
+    if (character === 'chef') chefDead = true;
+    if (character === 'maiden') maidenDead = true;
+    if (character === 'kid') kidDead = true;
+  }
+}
+
+function farmerPuppet(x, y) {
+  push();
+  imageMode(CENTER);
+
+  if (farmerDead) {
+    image(farmerDeadImage, x, y, 280 / 3, 716 / 3);
+  } else if (lipDistance > 5) {
+    image(farmerFireImage, x, y, 280 / 3, 716 / 3);
+  } else {
+    image(farmerImage, x, y, 280 / 3, 716 / 3);
+  }
+
+  pop();
+}
+function chefPuppet(x, y) {
+  push();
+  imageMode(CENTER);
+
+  if (chefDead) {
+    image(chefDeadImage, x, y, 318 / 3, 752  / 3);
+  } else if (lipDistance > 5) {
+    image(chefFireImage, x, y, 318 / 3, 752  / 3);
+  } else {
+    image(chefImage, x, y, 318 / 3, 752  / 3);
+  }
+
+  pop();
+}
+function maidenPuppet(x, y) {
+  push();
+  imageMode(CENTER);
+
+  if (maidenDead) {
+    image(maidenDeadImage, x, y, 301 / 3, 741 / 3);
+  } else if (lipDistance > 5) {
+    image(maidenFireImage, x, y, 301 / 3, 741 / 3);
+  } else {
+    image(maidenImage, x, y, 301 / 3, 741 / 3);
+  }
+
+  pop();
+}
+function kidPuppet(x, y) {
+  push();
+  imageMode(CENTER);
+
+  if (kidDead) {
+    image(kidDeadImage, x, y, 188 / 3, 465 / 3);
+  } else if (lipDistance > 5) {
+    image(kidFireImage, x, y, 188 / 3, 465 / 3);
+  } else {
+    image(kidImage, x, y, 188 / 3, 465 / 3);
+  }
+
+  pop();
 }
   //------------------------------------------------------
   // You can make addtional elements here, but keep the face drawing inside the for loop. 
-}
 function leftEye(face, colour) {
   let leftEyeValues = [33, 246, 161, 160, 159, 158, 157, 133, 155, 154, 153, 145, 144, 163, 7];
 
